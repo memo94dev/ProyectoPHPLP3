105b9c883c5dc156fb1a5b821917784d47f5fea2
Guillermo Home <guille.work.94@gmail.com>
2025-11-24 15:56:00 -0300

    Update view segun nivel de usuario

--- sidebar-menu.php
+++ sidebar-menu.php
@@ -1,5 +1,6 @@
 <?php
 
+// Sidebar menu para niveles de usuario Super Admin
 if (($_SESSION['permisos_acceso'] == 'Super Admin')) { ?>
     <ul class="sidebar-menu">
         <li class="header">Menu</li>
@@ -62,11 +63,11 @@ if (($_SESSION['permisos_acceso'] == 'Super Admin')) { ?>
         <!-- Administrar contraseñas -->
         <?php if ($_GET['module'] == "password") { ?>
             <li class="active">
-                <a href="?module=password"><i class="fa fa-user"></i>Cambiar Contraseña</a>
+                <a href="?module=password"><i class="fa fa-lock"></i>Cambiar Contraseña</a>
             </li>
         <?php } else { ?>
             <li>
-                <a href="?module=password"><i class="fa fa-user"></i>Cambiar Contraseña</a>
+                <a href="?module=password"><i class="fa fa-lock"></i>Cambiar Contraseña</a>
             </li>
         <?php } ?>
 
@@ -75,6 +76,7 @@ if (($_SESSION['permisos_acceso'] == 'Super Admin')) { ?>
 
     </ul>
 
+<!-- // Sidebar menu para niveles de usuario Compras -->
 <?php } else if (($_SESSION['permisos_acceso'] == 'Compras')) { ?>
 
     <ul class="sidebar-menu">
@@ -114,6 +116,86 @@ if (($_SESSION['permisos_acceso'] == 'Super Admin')) { ?>
                 </ul>
             </a>
         </li>
+
+        <!-- Administrar contraseñas también para Compras -->
+        <li class="<?php echo ($_GET['module'] == "password") ? 'active' : ''; ?>">
+            <a href="?module=password"><i class="fa fa-lock"></i>Cambiar Contraseña</a>
+        </li>
+    </ul>
+<?php } else if (($_SESSION['permisos_acceso'] == 'Compras')) { ?>
+    <ul class="sidebar-menu">
+        <li class="header">Menu</li>
+        <?php
+        if ($_GET['module'] == 'start') {
+            $active_home = 'active';
+        } else {
+            $active_home = '';
+        }
+        ?>
+        <li class="<?php echo $active_home; ?>">
+            <a href="?module=start"><i class="fa fa-home"></i>Inicio</a>
+        </li>
+        <li class="treeview">
+            <a href="javascript:void(0)">
+                <i class="fa fa-file-text"></i><span>Referenciales Generales</span><i class="fa fa-angle-left pull-right"></i>
+                <ul class="treeview-menu">
+                    <li><a href="?module=departamento"><i class="fa fa-circle-o"></i>Departamento</a></li>
+                    <li><a href="?module=ciudad"><i class="fa fa-circle-o"></i>Ciudad</a></li>
+                </ul>
+            </a>
+        </li>
+
     </ul>
 
+<?php } else if (($_SESSION['permisos_acceso'] == 'Ventas')) { ?>
+    <ul class="sidebar-menu">
+        <li class="header">Menu</li>
+        <?php
+        if ($_GET['module'] == 'start') {
+            $active_home = 'active';
+        } else {
+            $active_home = '';
+        }
+        ?>
+        <li class="<?php echo $active_home; ?>">
+            <a href="?module=start"><i class="fa fa-home"></i>Inicio</a>
+        </li>
+
+        <?php // if ($_GET['module'] == 'start') { 
+        ?>
+
+        <li class="treeview">
+            <a href="javascript:void(0)">
+                <i class="fa fa-file-text"></i><span>Referenciales Generales</span><i class="fa fa-angle-left pull-right"></i>
+                <ul class="treeview-menu">
+                    <li><a href="?module=departamento"><i class="fa fa-circle-o"></i>Departamento</a></li>
+                    <li><a href="?module=ciudad"><i class="fa fa-circle-o"></i>Ciudad</a></li>
+                </ul>
+            </a>
+        </li>
+
+        <li class="treeview">
+            <a href="javascript:void(0)">
+                <i class="fa fa-file-text"></i><span>Referenciales de Ventas</span><i class="fa fa-angle-left pull-right"></i>
+                <ul class="treeview-menu">
+                    <li><a href="?module=clientes"><i class="fa fa-circle-o"></i>Clientes</a></li>
+                </ul>
+            </a>
+        </li>
+        
+        <!-- Administrar contraseñas -->
+        <?php if ($_GET['module'] == "password") { ?>
+            <li class="active">
+                <a href="?module=password"><i class="fa fa-lock"></i>Cambiar Contraseña</a>
+            </li>
+        <?php } else { ?>
+            <li>
+                <a href="?module=password"><i class="fa fa-lock"></i>Cambiar Contraseña</a>
+            </li>
+        <?php } ?>
+
+        <?php //} 
+        ?>
+
+    </ul>
 <?php } ?>
