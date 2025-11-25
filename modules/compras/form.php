cfd306ba4d22be198e30c1c5b36aefbc0021b5ee
Guillermo Home <guille.work.94@gmail.com>
2025-11-24 21:05:39 -0300

    Add form compras primera parte

---
+++ modules/compras/form.php
@@ -0,0 +1,154 @@
+<!-- Insertar nueva Compra -->
+<?php if ($_GET['form'] == 'add') { ?>
+
+    <section class="content-header">
+        <h1>
+            <i class="fa fa-edit icon-title"></i> Cargar Compra
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="?module=start"><i class="fa fa-home"></i> Inicio</a></li>
+            <li><a href="?module=compras"> Compras</a></li>
+            <li class="active">Agregar</li>
+        </ol>
+    </section>
+
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <form role="form" class="form-horizontal" action="modules/compras/proses.php?act=insert" method="POST">
+                        <div class="box-body">
+                            <?php  // consulta para obtener el siguiente codigo de ciudad
+                            $query_id = mysqli_query($mysqli, "SELECT MAX(cod_compra) as id FROM compra")
+                                or die('Error : ' . mysqli_error($mysqli));
+                            $count = mysqli_num_rows($query_id);
+                            if ($count <> 0) {
+                                $data_id = mysqli_fetch_assoc($query_id);
+                                $codigo = $data_id['id'] + 1;
+                            } else {
+                                $codigo = 1;
+                            }
+                            ?>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <label class="col-sm-2 control-label">Codigo</label>
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control" name="cod_compra" value="<?php echo $codigo ?>" readonly>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <label class="col-sm-2 control-label">Fecha</label>
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control date-picker" name="fecha"
+                                            date-date-format="dd-mm-yyyy" placeholder="Ingrese la fecha"
+                                            value="<?php echo date("y-m-d") ?>" required readonly>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <label class="col-sm-2 control-label">Hora</label>
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control date-picker" name="hora"
+                                            date-date-format="dd-mm-yyyy" autocomplete="off"
+                                            value="<?php echo date("H:i:s") ?>" required readonly>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <label class="col-sm-2 control-label">Proveedror</label>
+                                    <div class="col-sm-5">
+                                        <select required class="chosen-select" data-placeholder="Seleccione un proveedor" autocomplete="off" name="cod_proveedor" id="">
+                                            <option value=""></option>
+                                            <?php
+                                            $query_prov = mysqli_query($mysqli, "SELECT cod_proveedor, razon_social, ruc 
+                                                                                FROM proveedor ORDER BY cod_proveedor ASC")
+                                                or die('Error: ' . mysqli_error($mysqli));
+                                            while ($data_prov = mysqli_fetch_assoc($query_prov)) {
+                                                echo "<option value=\"$data_prov[cod_proveedor]\"> $data_prov[razon_social] - $data_prov[ruc]</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <label class="col-sm-2 control-label">N de Factura</label>
+                                    <div class="col-sm-3">
+                                        <input maxlength="12" onkeypress="return goodchars(event, '0123456789-', this)" type="text" class="form-control" name="nro_factura" autocomplete="off" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <label class="col-sm-2 control-label">Deposito</label>
+                                    <div class="col-sm-5">
+                                        <select required class="chosen-select" data-placeholder="Seleccione un proveedor" autocomplete="off" name="cod_proveedor" id="">
+                                            <option value=""></option>
+                                            <?php
+                                            $query_dep = mysqli_query($mysqli, "SELECT cod_deposito, descrip 
+                                                                                FROM deposito ORDER BY cod_deposito ASC")
+                                                or die('Error: ' . mysqli_error($mysqli));
+                                            while ($data_dep = mysqli_fetch_assoc($query_dep)) {
+                                                echo "<option value=\"$data_dep[cod_deposito]\"> $data_dep[descrip]</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <label class="col-sm-2 control-label">Productos</label>
+                                    <div class="col-sm-3">
+                                        <button class="btn btn-info" data-toggle="modal" type="button"
+                                            data-target="#myModal">
+                                            <span class="glyphicon glyphicon-plus">Agregar Productos</span></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="resultados" class='col-md-9'>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php } ?>
+
+<div class="modal fade bs-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Buscar Productos</h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" action="">
+                    <div class="form-group">
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control" id="x" placeholder="Buscar por nombre o codigo" onkeyup="load(1);">
+                        </div>
+                        <button type="button" class="btn btn-default" onclick="load(1);">
+                            <span class="glyphicon glyphicon-search">Buscar</span>
+                        </button>
+                    </div>
+                </form>
+                <div id="loader" style="position: absolute; text-align: center; top: 55px; width: 100%; display:none"></div>
+                <div class="outer_div"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+
+</div>
